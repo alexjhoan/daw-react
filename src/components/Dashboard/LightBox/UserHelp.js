8e4eb7f08acd5f82f9c2b3abc5489f97694c6b12
alexjhoan <alexjhoan.25@gmail.com>
2020-08-27 18:34:47 -0400

    lista la platilla de ayuda del usuario 1830 27082020

--- src/components/Dashboard/LightBox/UserHelp.js
+++ src/components/Dashboard/LightBox/UserHelp.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import {Form, Button} from 'react-bootstrap'
 
 export default function UserHelp() {
   return (
-    <div>
-      <h1>Ayuda</h1>
-    </div>
+    <Form id="UserConfiguration" className="mt-4">
+      <div className="firstRow">
+        <p className="h4 mt-3">¿Tienes dudas sobre alguna sección de Dreamaway?</p>
+        <p>Tenemos una sección de preguntas frecuentes que quizás puedan resolver tus dudas en cuanto al funcionamiento de nuestra página y los servicios que presta.</p>
+        <a href="#" className="text-reset textYellow">Preguntas Frecuentes</a>
+        <p className="h4 mt-3">¿Tienes dudas sobre el manejo de tu información en Dreamaway?</p>
+        <p>Puedes leer nuestras políticas de uso de datos y privacidad para resolver tus dudas.</p>
+        <a href="#" className="text-reset textYellow">Políticas de uso de datos y privacidad</a>
+        <p className="h4 mt-3">¿Necesitas ayuda para resolver algún error dentro de Dreamaway?</p>
+        <p>Puedes ponerte en contacto con nosotros llenando el siguiente recuadro o escribiendo directamente a nuestro correo electrónico <a href="mailto:soporte@dreamaway.travel" target="_blank" className="text-reset textYellow">soporte@dreamaway.travel</a></p>
+        <Form.Group controlId="UserHelp">
+          <Form.Control as="textarea" />
+        </Form.Group>
+      </div>
+      <div className="SaveButtons d-flex align-items-center justify-content-center flex-md-row-reverse justify-content-md-start">
+        <Button
+          type="submit"
+          id="postProfile"
+          className="DawBtn"
+          >
+          Enviar
+        </Button>
+        <Button type="button" className="BtnCancel">
+          Descartar
+        </Button>
+      </div>
+    </Form>
   )
 }
--- src/components/Dashboard/LightBox/UserSecurity.js
+++ src/components/Dashboard/LightBox/UserSecurity.js
@@ -1,53 +1,59 @@
-import React from 'react'
-import {Form, Button} from 'react-bootstrap'
-
+import React from "react"
+import {Form, Button} from "react-bootstrap"
 import LockIcon from "../../../assets/images/icons/i_lock.svg"
 import InputAdornment from "@material-ui/core/InputAdornment/InputAdornment"
-import {FancyAutocomplete, FancyTextInput} from "../../../components/FancyComponents"
-import {useForm, Controller} from 'react-hook-form'
-import * as yup from 'yup';
-import { newPassword } from '../../../services/AuthService';
-import { useDispatch, useSelector } from 'react-redux';
-
+import {
+  FancyAutocomplete,
+  FancyTextInput
+} from "../../../components/FancyComponents"
+import {useForm, Controller} from "react-hook-form"
+import * as yup from "yup"
+import {newPassword} from "../../../services/AuthService"
+import {useDispatch, useSelector} from "react-redux"
 import {useHistory} from "react-router-dom"
-import IconButton from '@material-ui/core/IconButton';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import IconButton from "@material-ui/core/IconButton"
+import Visibility from "@material-ui/icons/Visibility"
+import VisibilityOff from "@material-ui/icons/VisibilityOff"
 
 const schema = yup.object().shape({
   password: yup.string().required(), //wait for validation length
-  passwordConfirm: yup.string().required(), //wait for validation length
+  passwordConfirm: yup.string().required() //wait for validation length
 })
 
-export default function UserInterests() {
-  const history = useHistory();
-  const dispatch = useDispatch();
+export default function UserSecurity() {
+  const history = useHistory()
+  const dispatch = useDispatch()
   const methods = useForm({
     validationSchema: schema
   })
   const {handleSubmit, watch, errors, register, control} = methods
   const onSubmit = (formData: any) => {
-    const userData= {
-      password: formData.password,
+    const userData = {
+      password: formData.password
     }
     console.log(userData)
     newPassword(userData, history, dispatch)
   }
-  const [valuePassword, setValuePassword] = React.useState({showPassword: false,})
+  const [valuePassword, setValuePassword] = React.useState({
+    showPassword: false
+  })
   const handleClickShowPassword = () => {
-    setValuePassword({ showPassword: !valuePassword.showPassword });
+    setValuePassword({showPassword: !valuePassword.showPassword})
   }
   return (
     <form
       id="UserConfiguration"
       onSubmit={handleSubmit(onSubmit)}
-      className="formRestore col-md-12 center-block px-0 mt-4"
-      >
+      className="formRestore col-md-12 center-block px-0 mt-4">
       <div className="firstRow">
         <p className="h4">Cambio de contraseña:</p>
-        <p>Haz que tu contraseña sea segura y no la compartas con nadie, de esa forma podemos asegurarnos de que tus datos están a salvo en nuestras manos.</p>
+        <p>
+          Haz que tu contraseña sea segura y no la compartas con nadie, de esa
+          forma podemos asegurarnos de que tus datos están a salvo en nuestras
+          manos.
+        </p>
         <div className="font-weight-light text-help">
-         Por favor ingresa tu contraseña actual
+          Por favor ingresa tu contraseña actual
         </div>
         <div className="form-group w-sm-50">
           <Controller
@@ -66,16 +72,19 @@ export default function UserInterests() {
                 <IconButton
                   aria-label="toggle password visibility"
                   onClick={handleClickShowPassword}
-                  edge="end"
-                >
-                  {valuePassword.showPassword ? <Visibility /> : <VisibilityOff />}
+                  edge="end">
+                  {valuePassword.showPassword ? (
+                    <Visibility />
+                  ) : (
+                    <VisibilityOff />
+                  )}
                 </IconButton>
               )
             }}
           />
         </div>
         <div className="font-weight-light text-help">
-         Por favor ingresa tu nueva contraseña
+          Por favor ingresa tu nueva contraseña
         </div>
         <div className="form-group w-sm-50">
           <Controller
@@ -95,16 +104,19 @@ export default function UserInterests() {
                 <IconButton
                   aria-label="toggle password visibility"
                   onClick={handleClickShowPassword}
-                  edge="end"
-                >
-                  {valuePassword.showPassword ? <Visibility /> : <VisibilityOff />}
+                  edge="end">
+                  {valuePassword.showPassword ? (
+                    <Visibility />
+                  ) : (
+                    <VisibilityOff />
+                  )}
                 </IconButton>
               )
             }}
           />
         </div>
         <div className="font-weight-light text-help">
-         confirma tu nueva contraseña
+          confirma tu nueva contraseña
         </div>
         <div className="form-group w-sm-50">
           <Controller
@@ -124,11 +136,14 @@ export default function UserInterests() {
                 <IconButton
                   aria-label="toggle password visibility"
                   onClick={handleClickShowPassword}
-                  edge="end"
-                >
-                  {valuePassword.showPassword ? <Visibility /> : <VisibilityOff />}
+                  edge="end">
+                  {valuePassword.showPassword ? (
+                    <Visibility />
+                  ) : (
+                    <VisibilityOff />
+                  )}
                 </IconButton>
-              ),
+              )
             }}
           />
         </div>
@@ -139,9 +154,8 @@ export default function UserInterests() {
           className="DawBtn"
           disabled={
             !watch("password" || errors.password) ||
-            (watch('password') != watch('passwordConfirm'))
-          }
-        >
+            watch("password") != watch("passwordConfirm")
+          }>
           Cambiar contraseña
         </button>
         <Button type="button" className="BtnCancel">
